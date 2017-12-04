851f62fe65ba9d9847bbf14fb50feae50b892169
gelos <9svtcrze9b2m@mail.ru>
2017-12-04 16:57:26 +0300

    Release Set with array, complete DisjointSets

--- DisjointSets.java
+++ DisjointSets.java
@@ -14,14 +14,14 @@ public class DisjointSets<T> {
     sets = new ArrayList<Set<T>>();
 
     // init parent storage
-    p = new int[data.size()];
+    //p = new int[data.size()];
     
-    // init size storage
+    // init parent storage
     s = new int[data.size()];
 
     for (int i = 0; i < data.size(); ++i) {
 
-      // fill sets storage
+      // fill sets storage, one set - one element
       T element = data.get(i);
       Set<T> newset = new Set<T>(element);
       this.sets.add(newset);
@@ -30,6 +30,7 @@ public class DisjointSets<T> {
       s[i] = i;
 
     }
+    this.setsCount = data.size();
   }
 
   // Release union by size algo 
@@ -37,38 +38,58 @@ public class DisjointSets<T> {
   
   public int union(int root1, int root2) {
     
+    if (root1 == root2) {
+      return root1;
+    }
     
-    
-    return
-    
+    // union small set to big 
+    if (this.sets.get(root1).size() > this.sets.get(root2).size()) {
+      s[root2] = root1;
+      this.sets.get(root1).addAll(this.sets.get(root2));
+      this.sets.get(root2).clear();
+      this.setsCount--;
+      return root1;
+    } else {
+      s[root1] = root2;
+      this.sets.get(root2).addAll(this.sets.get(root1));
+      this.sets.get(root1).clear();
+      this.setsCount--;
+      return root2
+    }
+            
   }
 
   // Must implement path compression
   public int find(int x) {
-    return -1; // TODO: remove and replace this line
+    //return -1; // TODO: remove and replace this line
+    if (s[x] == x) {
+      return x;
+    } else {
+      s[x] = find(s[x]);
+    }
+    return s[x];
   }
 
   // Get all the data in the same set
   // Must have O(1) time complexity
   public Set<T> get(int root) {
-    return null; // TODO: remove and replace this line
-
+    return this.sets.get(root);
   }
 
   // return the number of disjoint sets remaining
   // must be O(1) time
   public int getNumSets() {
-    return sets.size();
+    //return sets.size();
+    return setsCount;
   }
 
   // Data
 
-  // size storage
-  private int[] s;
+  // sets count
+  private int setsCount;
   
+  // parent storage
+  private int[] s;
   // sets storage
   private ArrayList<Set<T>> sets;
-
-  // parents storage
-  private int[] p;
 }
--- Set.java
+++ Set.java
@@ -4,69 +4,80 @@
 // This is used in DisjointSets<T> to store actual data in the same sets
 //
 
-//You cannot import additonal items
+// You cannot import additonal items
 import java.util.AbstractCollection;
-import java.util.ArrayList;
 import java.util.Iterator;
-//You cannot import additonal items
+// You cannot import additonal items
 
 //
-//Hint: if you think really hard, you will realize this class Set<T> is in fact just a list
-//      because DisjointSets<T> ensures that all values stored in Set<T> must be unique, 
-//      but should it be array list or linked list??
+// Hint: if you think really hard, you will realize this class Set<T> is in fact just a list
+// because DisjointSets<T> ensures that all values stored in Set<T> must be unique,
+// but should it be array list or linked list??
 //
 
-public class Set<T> extends AbstractCollection<T>
-{
-    // Constructor
+//http://grepcode.com/file/repository.grepcode.com/java/root/jdk/openjdk/6-b14/java/util/ArrayList.java#ArrayList
+
+
+public class Set<T> extends AbstractCollection<T> {
+
+  // Consructor
   
-    public Set() {
-      data = new ArrayList<T>();
+  public Set() {
+    this.data = new int[10];
+    this.size = 0;
+  }
+  
+  // O(1)
+  public boolean add(T item) {
+
+    return data.add(item);
+  }
+
+  // O(1)
+  public boolean addAll(Set<T> other) {
+    return data.addAll(other);
+  }
+
+  // O(1)
+  public void clear() {
+    data.clear();
+  }
+
+  // O(1)
+  public int size() {
+    return data.size();
+  }
+
+  public Iterator<T> iterator() {
+    return new Iterator<T>() {
+      private Iterator<T> iter = data.iterator();
+
+      public T next() {
+        // return null;
+        return iter.next();
+      }
+
+      public boolean hasNext() {
+        // return false;
+        return iter.hasNext();
+      }
+    };
+  }
+
+  // Data
+  private T[] data;
+  private int size = 0;
+
+  private void ensureCapacity(int minCapacity) {
+    modCount++;
+    int oldCapacity = elementData.length;
+    if (minCapacity > oldCapacity) {
+      Object oldData[] = elementData;
+      int newCapacity = (oldCapacity * 3) / 2 + 1;
+      if (newCapacity < minCapacity)
+        newCapacity = minCapacity;
+      // minCapacity is usually close to size, so this is a win:
+      elementData = Arrays.copyOf(elementData, newCapacity);
     }
-    
-	//O(1)
-	public boolean add(T item)
-	{
-		return data.add(item);
-	}
-	
-	//O(1)
-	public boolean addAll(Set<T> other)
-	{
-		return data.addAll(other);
-	}
-	
-	//O(1)
-	public void clear()
-	{
-		data.clear();
-	}
-	
-	//O(1)
-	public int size()
-	{
-		return data.size();
-	}
-	
-	public Iterator<T> iterator()
-	{
-		return new Iterator<T>()
-		{
-		    private Iterator<T> iter = data.iterator();
-			public T next()
-			{
-				//return null;
-			    return iter.next();
-			}
-			
-			public boolean hasNext()
-			{
-				//return false;
-			  return iter.hasNext();
-			}
-		};
-	}
-	
-	// Data
-	private ArrayList<T> data;
+  }
 }
